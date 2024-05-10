fc93fda46d76019204a314632ca33c2f42a3286d
NguyenDangViet2005 <vietnguyen.1022005@gmail.com>
2024-05-10 18:45:17 +0700

    video 19

--- src/components/Mycomponents.js
+++ src/components/Mycomponents.js
@@ -9,11 +9,22 @@ class Mycomponent extends React.Component {
     age: 19,
     address: "quang nam",
   };
+  HandleClick(event) {
+    console.log("anh là nguyễn đăng việt đây hahahah");
+    // console.log(event.target);
+    // in ra event khi thao tác với web
+  }
+  HandleMouseOver(event) {
+    console.log(event.target);
+  }
   //JSX
   render() {
     return (
       <div>
         my name is {this.state.name} and i am from {this.state.address}
+        <button onClick={this.HandleClick}>Click me</button>
+        <button onMouseOver={this.HandleMouseOver}>Hover me</button>
+        {/* không cần phải HandleClick() khi gọi hàm */}
       </div>
     );
     //Javascipt được định nghĩa bằng cặp dấu {}
